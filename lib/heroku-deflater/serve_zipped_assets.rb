24a88bacb1d35a3197143578e5de3241194c5a8d
Daniel Greenlaw <daniel@shirtspace.com>
2021-01-15 15:02:11 -0800

    Adapt handling of compressed assets for use with Rails 6.1

--- lib/heroku-deflater/serve_zipped_assets.rb
+++ lib/heroku-deflater/serve_zipped_assets.rb
@@ -10,6 +10,9 @@ module HerokuDeflater
   class ServeZippedAssets
     def initialize(app, root, paths, cache_control_manager)
       @app = app
+
+      return if HerokuDeflater.rails_version_6_1?
+
       @paths = paths.map { |p| p.chomp('/') + '/' }
 
       if HerokuDeflater.rails_version_5?
@@ -20,38 +23,36 @@ module HerokuDeflater
     end
 
     def call(env)
-      if env['REQUEST_METHOD'] == 'GET'
-        request = Rack::Request.new(env)
-        encoding = Rack::Utils.select_best_encoding(%w(gzip identity), request.accept_encoding)
+      @app.call(env) if HerokuDeflater.rails_version_6_1? || env['REQUEST_METHOD'] != 'GET'
 
-        if encoding == 'gzip'
-          # See if gzipped version exists in assets directory
-          compressed_path = env['PATH_INFO'] + '.gz'
+      request = Rack::Request.new(env)
+      encoding = Rack::Utils.select_best_encoding(%w(gzip identity), request.accept_encoding)
 
-          if compressed_path.start_with?(*@paths) && (match = @file_handler.match?(compressed_path))
-            # Get the FileHandler to serve up the gzipped file, then strip the .gz suffix
-            env['PATH_INFO'] = match
-            status, headers, body = @file_handler.call(env)
-            env['PATH_INFO'].chomp!('.gz')
+      if encoding == 'gzip'
+        # See if gzipped version exists in assets directory
+        compressed_path = env['PATH_INFO'] + '.gz'
 
-            # Set the Vary HTTP header.
-            vary = headers['Vary'].to_s.split(',').map(&:strip)
+        if compressed_path.start_with?(*@paths) && (match = @file_handler.match?(compressed_path))
+          # Get the FileHandler to serve up the gzipped file, then strip the .gz suffix
+          env['PATH_INFO'] = match
+          status, headers, body = @file_handler.call(env)
+          env['PATH_INFO'].chomp!('.gz')
 
-            unless vary.include?('*') || vary.include?('Accept-Encoding')
-              headers['Vary'] = vary.push('Accept-Encoding').join(',')
-            end
+          # Set the Vary HTTP header.
+          vary = headers['Vary'].to_s.split(',').map(&:strip)
 
-            # Add encoding and type
-            headers['Content-Encoding'] = 'gzip'
-            headers['Content-Type'] = Rack::Mime.mime_type(File.extname(env['PATH_INFO']), 'text/plain')
-
-            body.close if body.respond_to?(:close)
-            return [status, headers, body]
+          unless vary.include?('*') || vary.include?('Accept-Encoding')
+            headers['Vary'] = vary.push('Accept-Encoding').join(',')
           end
+
+          # Add encoding and type
+          headers['Content-Encoding'] = 'gzip'
+          headers['Content-Type'] = Rack::Mime.mime_type(File.extname(env['PATH_INFO']), 'text/plain')
+
+          body.close if body.respond_to?(:close)
+          return [status, headers, body]
         end
       end
-
-      @app.call(env)
     end
   end
 end
